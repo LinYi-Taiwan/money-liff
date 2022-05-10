36df9558c4b82dd4cbd7c3d17aef08c4640c7b80
林易 <linyi@linyideMacBook-Pro.local>
2022-05-10 21:39:42 +0800

    fix: line flex box adjustment

--- src/layouts/LineOnly/index.js
+++ src/layouts/LineOnly/index.js
@@ -30,7 +30,7 @@ export const flexBox = ({ arrivedTime, destination }) => {
             paddingAll: "20px",
             backgroundColor: "#0367D3",
             spacing: "md",
-            height: "154px",
+            height: "100px",
             paddingTop: "22px",
         },
         body: {
@@ -129,14 +129,14 @@ export const flexBox = ({ arrivedTime, destination }) => {
                             ],
                             width: "12px",
                         },
-                        // {
-                        //     type: "text",
-                        //     text: "Walk 4min",
-                        //     gravity: "center",
-                        //     flex: 4,
-                        //     size: "xs",
-                        //     color: "#8c8c8c",
-                        // },
+                        {
+                            type: "text",
+                            text: "Drive 40min",
+                            gravity: "center",
+                            flex: 4,
+                            size: "xs",
+                            color: "#8c8c8c",
+                        },
                     ],
                     spacing: "lg",
                     height: "64px",
@@ -151,7 +151,7 @@ export const flexBox = ({ arrivedTime, destination }) => {
                             contents: [
                                 {
                                     type: "text",
-                                    text: "20:34",
+                                    text: arrivedTime,
                                     gravity: "center",
                                     size: "sm",
                                 },
@@ -191,59 +191,6 @@ export const flexBox = ({ arrivedTime, destination }) => {
                     ],
                     spacing: "lg",
                     cornerRadius: "30px",
-                },
-                {
-                    type: "box",
-                    layout: "horizontal",
-                    contents: [
-                        {
-                            type: "box",
-                            layout: "baseline",
-                            contents: [
-                                {
-                                    type: "filler",
-                                },
-                            ],
-                            flex: 1,
-                        },
-                        {
-                            type: "box",
-                            layout: "vertical",
-                            contents: [
-                                {
-                                    type: "box",
-                                    layout: "horizontal",
-                                    contents: [
-                                        {
-                                            type: "filler",
-                                        },
-                                        {
-                                            type: "box",
-                                            layout: "vertical",
-                                            contents: [],
-                                            width: "2px",
-                                            backgroundColor: "#6486E3",
-                                        },
-                                        {
-                                            type: "filler",
-                                        },
-                                    ],
-                                    flex: 1,
-                                },
-                            ],
-                            width: "12px",
-                        },
-                        // {
-                        //     type: "text",
-                        //     text: "Metro 1hr",
-                        //     gravity: "center",
-                        //     flex: 4,
-                        //     size: "xs",
-                        //     color: "#8c8c8c",
-                        // },
-                    ],
-                    spacing: "lg",
-                    height: "64px",
                 },
             ],
         },
